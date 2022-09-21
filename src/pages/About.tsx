ffa2706ad139234d0c2512bc81fa3db778ad9bbc
boryswnr <borysweiner@gmail.com>
2022-09-21 12:50:59 +0200

    added info to home and about pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,19 @@
 export function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About</h1>
+      <div className="fs-5">
+        <p>
+          This is a shopping cart project. It uses React with TypeScript, styled
+          with bootstrap. To see how it works, go to the STORE
+          webiste.
+        </p>
+        <p>
+          You can add items to cart, increase and decrease the amount you have,
+          usual stuff. Also, the contents of the cart are saved in seesion
+          storage.
+        </p>
+      </div>
+    </>
+  );
 }
